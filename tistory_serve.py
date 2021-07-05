00c6314106b56c14eacbbad0ccef5bdf188f7e3b
miyeonha <miyeon.ha.22@gmail.com>
2021-07-05 20:50:53 +0900

    TODO : add Flask cache

--- tistory_serve.py
+++ tistory_serve.py
@@ -1,9 +1,19 @@
 import json
 
 from flask import Flask, render_template, request
+from flask_caching import Cache
+
 import requests
 
+config = {
+    "DEBUG": True,  # some Flask specific configs
+    "CACHE_TYPE": "simple",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300,
+}
+
 app = Flask(__name__)
+app.config.from_mapping(config)
+cache = Cache(app)
 
 
 def _load_json(input_path):
@@ -36,7 +46,7 @@ def _get_blog_info(access_token):
 
     URL = f"https://www.tistory.com/apis/blog/info?access_token={access_token}&output=json"
 
-    blog_info = request.get(url=URL).json()
+    blog_info = requests.get(url=URL).json()
 
     try:
         if blog_info["tistory"]["status"] == "200":
@@ -76,12 +86,20 @@ def _get_categories(access_token, blog_name):
         return "fail"
 
 
-@app.route("/write-post")
+@app.route("/write-post", methods=["POST"])
+@cache.cached(timeout=50)
 def write_post():
+
+    selected_blog_name = request.form["select-blog-name"]
+
+    print("selected_blog_name : ")
+    print(selected_blog_name)
+
     return render_template("write-post.html")
 
 
 @app.route("/")
+@cache.cached(timeout=50)
 def index():
 
     auth = _load_json("./tistory_auth.json")
@@ -90,17 +108,29 @@ def index():
     redirect_uri = auth["redirect_uri"]
 
     parameter_dict = request.args.to_dict()
-    result_access_token = _get_access_token(
-        parameter_dict, client_id, redirect_uri, client_secret
-    )
 
+    result_access_token = None
     list_blog_names = None
-    if result_access_token != "fail":
-        list_blogs = _get_blog_info(result_access_token)
-        list_blog_names = _convert_blog_name(list_blogs)
+
+    if parameter_dict and result_access_token is None:
+        result_access_token = _get_access_token(
+            parameter_dict, client_id, redirect_uri, client_secret
+        )
+
+        list_blog_names = None
+        if result_access_token != "fail":
+            list_blogs = _get_blog_info(result_access_token)
+
+            print("list_blogs : ")
+            print(json.dumps(list_blogs, ensure_ascii=False, indent=2))
+
+            list_blog_names = _convert_blog_name(list_blogs)
+
+            print("list_blog_names : ")
+            print(json.dumps(list_blog_names, ensure_ascii=False, indent=2))
 
     return render_template(
-        "tistory_index.html",
+        "tistory.html",
         client_id=client_id,
         redirect_uri=redirect_uri,
         result_access_token=result_access_token,
